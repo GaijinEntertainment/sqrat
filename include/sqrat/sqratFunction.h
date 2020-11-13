02a2b170d66ca20885896c41dbbf6d27224621a9
VasiliyRyabtsev <vasiliy.ryabtsev@gmail.com>
2020-11-13 20:47:01 +0300

    Remove empty string of tabs added in last commit
    No need for trailing spaces and mixing identation of spaces and tabs

--- include/sqrat/sqratFunction.h
+++ include/sqrat/sqratFunction.h
@@ -238,7 +238,6 @@ public:
     template <typename... Args>
     bool Execute(Args const&... args) const {
         static constexpr size_t nArgs = sizeof...(Args);
-		
         SQInteger top = sq_gettop(vm);
 
         sq_pushobject(vm, obj);
